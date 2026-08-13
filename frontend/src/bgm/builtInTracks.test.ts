9b76e58f414f5869a512d7c1f79a5de52cd89f33
LoShell <241250088@smail.nju.edu.cn>
2026-08-14 01:08:33 +0800

    test: 同步内置 BGM 文件列表

--- frontend/src/bgm/builtInTracks.test.ts
+++ frontend/src/bgm/builtInTracks.test.ts
@@ -5,7 +5,7 @@ describe("builtInTracks", () => {
   it("registers the demo audio files as playable built-in tracks", () => {
     expect(builtInTracks.map((track) => track.fileRef)).toEqual([
       "/bgm/DearJohn.mp3",
-      "/bgm/Untouchabel.mp3",
+      "/bgm/爱的供养.mp3",
       "/bgm/Valhalla.mp3",
       "/bgm/岸边客.mp3",
       "/bgm/招摇.mp3",
